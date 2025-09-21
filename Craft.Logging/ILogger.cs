3db66fd1f032fff3284f74ca54f75fd274c826e9
EbbeMeloSorensen <ebbe.melo.sorensen@gmail.com>
2025-09-21 18:03:44 +0200

    -

---
+++ Craft.Logging/ILogger.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Craft.Logging
+{
+    public enum LogMessageCategory
+    {
+        Debug,
+        Information,
+        Warning,
+        Error,
+        Fatal
+    }
+
+    public interface ILogger
+    {
+        string WriteLine(
+            LogMessageCategory category,
+            string message,
+            string aspect = "general",
+            bool startStopwatch = false);
+    }
+}
