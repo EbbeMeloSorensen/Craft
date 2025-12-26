f3ac451033a16bddfa33d1bd536b7fec71291685
Ebbe Melo Sorensen <ebbe.melo.sorensen@gmail.com>
2025-12-26 15:53:53 +0100

    -

--- Craft.Simulation.GuiTest/Tab1/Tab1ViewModel.cs
+++ Craft.Simulation.GuiTest/Tab1/Tab1ViewModel.cs
@@ -5,16 +5,46 @@ using Craft.Simulation.Boundaries;
 using Craft.ViewModels.Geometry2D.ScrollFree;
 using Craft.ViewModels.Simulation;
 using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
 
 namespace Craft.Simulation.GuiTest.Tab1
 {
     public class Tab1ViewModel : ViewModelBase
     {
         private SceneViewController _sceneViewController;
+        private RelayCommand _startAnimationCommand;
+        private RelayCommand _pauseAnimationCommand;
+        private string _startResumeButtonText = "Start";
 
         public Engine.Engine Engine { get; }
 
         public GeometryEditorViewModel GeometryEditorViewModel { get; }
+
+        public RelayCommand StartAnimationCommand
+        {
+            get
+            {
+                return _startAnimationCommand ?? (_startAnimationCommand = new RelayCommand(StartAnimation, CanStartAnimation));
+            }
+        }
+
+        public RelayCommand PauseAnimationCommand
+        {
+            get
+            {
+                return _pauseAnimationCommand ?? (_pauseAnimationCommand = new RelayCommand(PauseAnimation, CanPauseAnimation));
+            }
+        }
+
+        public string StartResumeButtonText
+        {
+            get => _startResumeButtonText;
+            set
+            {
+                _startResumeButtonText = value;
+                RaisePropertyChanged();
+            }
+        }
 
         public Tab1ViewModel()
         {
@@ -35,7 +65,7 @@ namespace Craft.Simulation.GuiTest.Tab1
                 false);
 
             _sceneViewController.ActiveScene = scene;
-            Engine.StartOrResumeAnimation();
+            //Engine.StartOrResumeAnimation();
         }
 
         private Scene GenerateScene()
@@ -76,6 +106,31 @@ namespace Craft.Simulation.GuiTest.Tab1
             scene.AddBoundary(new HalfPlane(new Vector2D(-1, 1), new Vector2D(1, 0)));
 
             return scene;
+        }
+
+        private void StartAnimation()
+        {
+            Engine.StartOrResumeAnimation();
+            StartAnimationCommand.RaiseCanExecuteChanged();
+            PauseAnimationCommand.RaiseCanExecuteChanged();
+        }
+
+        private bool CanStartAnimation()
+        {
+            return !Engine.AnimationRunning;
+        }
+
+        private void PauseAnimation()
+        {
+            Engine.PauseAnimation();
+            StartResumeButtonText = "Resume";
+            StartAnimationCommand.RaiseCanExecuteChanged();
+            PauseAnimationCommand.RaiseCanExecuteChanged();
+        }
+
+        private bool CanPauseAnimation()
+        {
+            return Engine.AnimationRunning;
         }
     }
 }
