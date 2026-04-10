f49aeab2148f6e4c7d1c5b4c8771cc23f4152685
EbbeMeloSorensen <ebbe.melo.sorensen@gmail.com>
2026-04-10 16:48:41 +0200

    -

---
+++ Craft.UIElements.Reborn.GuiTest/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Craft.UIElements.Reborn.GuiTest
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ Craft.ViewModels/Geometry2D/Reborn/LineModel.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows;
+
+namespace Craft.ViewModels.Geometry2D.Reborn;
+
+public class LineModel
+{
+    public Point P1 { get; set; }
+    public Point P2 { get; set; }
+}
