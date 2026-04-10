29d3d91354e10cdf030e68597cf3e0f5dec6184e
EbbeMeloSorensen <ebbe.melo.sorensen@gmail.com>
2026-04-10 16:56:25 +0200

    -

--- Craft.UIElements.Reborn.GuiTest/MainWindow.xaml.cs
+++ Craft.UIElements.Reborn.GuiTest/MainWindow.xaml.cs
@@ -19,6 +19,8 @@ namespace Craft.UIElements.Reborn.GuiTest
         public MainWindow()
         {
             InitializeComponent();
+
+            DataContext = new MainWindowViewModel();
         }
     }
 }
---
+++ Craft.UIElements.Reborn.GuiTest/MainWindowViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Craft.ViewModels.Geometry2D.Reborn;
+
+namespace Craft.UIElements.Reborn.GuiTest
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public GeometryViewModel GeometryViewModel { get; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public MainWindowViewModel()
+        {
+            GeometryViewModel = new GeometryViewModel();
+        }
+
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
+}
