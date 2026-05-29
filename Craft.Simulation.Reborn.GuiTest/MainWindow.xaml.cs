664cdb2bd8e90d1fd92ed0079d25bfcd21d9070e
Ebbe Melo Sorensen <ebbe.melo.sorensen@gmail.com>
2026-05-29 19:32:52 +0200

    -

---
+++ Craft.Simulation.Reborn.GuiTest/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel;
+using System.Windows;
+
+namespace Craft.Simulation.Reborn.GuiTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private MainWindowViewModel ViewModel { get { return DataContext as MainWindowViewModel; } }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            DataContext = new MainWindowViewModel();
+        }
+
+        private void MainWindow_OnClosing(
+            object sender,
+            CancelEventArgs e)
+        {
+            ViewModel.Engine.HandleClosing();
+        }
+    }
+}
